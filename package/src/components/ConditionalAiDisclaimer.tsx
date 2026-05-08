b437e04674d7946948523073eeeabe837c64e38f
Andrea Davide <develop.devde@gmail.com>
2026-05-08 11:29:06 +0200

    ml

--- package/src/components/ConditionalAiDisclaimer.tsx
+++ package/src/components/ConditionalAiDisclaimer.tsx
@@ -11,11 +11,11 @@ export default function ConditionalAiDisclaimer() {
 
   return (
     <div className="relative z-20 w-full px-12 xl:px-0 py-7">
-      <div className="mx-auto w-full max-w-[1700px] 2xl:px-8">
+      <div className="mx-auto w-full max-w-[1700px] 2xl:px-8 ml-12">
         <p className="text-white/75 text-sm text-left">
           Not an actual patient. Visuals created with the help of AI.
         </p>
       </div>
     </div>
-  )
+  );
 }
